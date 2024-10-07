df216fcab80067b7188896418a9911d82fadf1d5
someonecantcode <s-btea@lwsd.org>
2024-10-06 19:05:30 -0700

    cron testing

--- committer.py
+++ committer.py
@@ -1,4 +1,4 @@
-import requests, base64, json, os
+import requests, base64, json, os, random, time
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -18,10 +18,11 @@ def updateFile(data: str, msg: str, API_KEY: str) -> requests.Response:
     }
     return requests.put('https://api.github.com/repos/someonecantcode/auto-committer/contents/data', auth=("someonecantcode", API_KEY), data=json.dumps(payload))
 
+random.seed(time.time())
 API_KEY: str = os.getenv("API_KEY")
-content: str = "hmm ok"
-msg: str = "har2"
+content: str =  os.getenv("original_msg") +"\n" + str(random.random()) 
+msg: str = "hip hip horay"
 
 r = updateFile(content, msg, API_KEY)
+print(r.status_code)
 # print(json.loads(r.content))
-# print(r.status_code)
